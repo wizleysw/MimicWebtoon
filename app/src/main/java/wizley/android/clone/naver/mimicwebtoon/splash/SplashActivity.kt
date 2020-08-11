6487ce99cba39b3a94abaf1996d5bb218bda692b
wizleysw <wizley@kakao.com>
2020-08-11 20:12:10 +0900

    edit gitignore

---
+++ app/src/main/java/wizley/android/clone/naver/mimicwebtoon/splash/SplashActivity.kt
@@ -0,0 +1,11 @@
+package wizley.android.clone.naver.mimicwebtoon.splash
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
