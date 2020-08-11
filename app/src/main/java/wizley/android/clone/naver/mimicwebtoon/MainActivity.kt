2e263ad1bd7d0bb6ceedca9c82ddc4cd41c5916f
wizleysw <wizley@kakao.com>
2020-08-11 22:01:27 +0900

    add MainActivity layout with BottomNavigationView
    Reviewed by Wizley.
    2020.08.11.
    
    BottomNavigationView is helpful when moving to another activity by onClick method. I have disabled tint mode on menu item and adeed icon to each menu.

--- app/src/main/java/wizley/android/clone/naver/mimicwebtoon/MainActivity.kt
+++ app/src/main/java/wizley/android/clone/naver/mimicwebtoon/MainActivity.kt
@@ -2,10 +2,75 @@ package wizley.android.clone.naver.mimicwebtoon
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.MenuItem
+import android.view.View
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import wizley.android.clone.naver.mimicwebtoon.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener,
+    View.OnLongClickListener {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.gnbBar.itemIconTintList = null
+        binding.gnbBar.setOnNavigationItemSelectedListener(this)
+
+        setGnbColor()
+    }
+
+    override fun onNavigationItemSelected(menu: MenuItem): Boolean {
+        when(menu.itemId){
+            R.id.menu_gnb_webtoon -> {
+
+            }
+            R.id.menu_gnb_recommend -> {
+
+            }
+            R.id.menu_gnb_bestchallenge -> {
+
+            }
+            R.id.menu_gnb_my -> {
+
+            }
+            R.id.menu_gnb_more -> {
+
+            }
+        }
+        return false
+    }
+
+    fun setGnbColor(){
+        val selected = binding.gnbBar.selectedItemId
+        val selectedMenu = binding.gnbBar.menu.findItem(selected)
+        when(selectedMenu.itemId){
+            R.id.menu_gnb_webtoon -> {
+                selectedMenu.setIcon(R.drawable.menu_gnb_webtoon_on)
+            }
+            R.id.menu_gnb_recommend -> {
+                selectedMenu.setIcon(R.drawable.menu_gnb_recommendfinish_on)
+            }
+            R.id.menu_gnb_bestchallenge -> {
+                selectedMenu.setIcon(R.drawable.menu_gnb_bestchallenge_on)
+            }
+            R.id.menu_gnb_my -> {
+                selectedMenu.setIcon(R.drawable.menu_gnb_my_on)
+            }
+            R.id.menu_gnb_more -> {
+                selectedMenu.setIcon(R.drawable.menu_gnb_more_on)
+            }
+        }
+
+    }
+
+    override fun onLongClick(v: View?): Boolean {
+        Log.e("here", v?.id.toString())
+        return true
     }
 }
--- app/src/main/java/wizley/android/clone/naver/mimicwebtoon/splash/SplashActivity.kt
+++ app/src/main/java/wizley/android/clone/naver/mimicwebtoon/splash/SplashActivity.kt
@@ -1,11 +1,26 @@
 package wizley.android.clone.naver.mimicwebtoon.splash
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
+import wizley.android.clone.naver.mimicwebtoon.MainActivity
+import wizley.android.clone.naver.mimicwebtoon.databinding.ActivitySplashBinding
 
 class SplashActivity: AppCompatActivity() {
 
+    private lateinit var binding: ActivitySplashBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        Handler(Looper.getMainLooper()).postDelayed(Runnable {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }, 2000)
     }
 }
