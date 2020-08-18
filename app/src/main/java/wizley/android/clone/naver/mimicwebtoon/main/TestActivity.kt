ac28f4899492c055a61977b809a66edbdb6481ab
wizleysw <wizley@kakao.com>
2020-08-18 20:10:52 +0900

    add bottom ImageBanner with ConstraintSet
    Reviewed by Wizley.
    2020.08.18.
    
    ImageBanner is hide/show when user scrolls nestedview. So implemented using constraintSet to programically change constraint pos.

--- app/src/main/java/wizley/android/clone/naver/mimicwebtoon/main/TestActivity.kt
+++ app/src/main/java/wizley/android/clone/naver/mimicwebtoon/main/TestActivity.kt
@@ -4,7 +4,9 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import android.util.Log
 import android.view.Menu
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintSet
 import androidx.core.widget.NestedScrollView
 import kotlinx.android.synthetic.main.test.*
 import wizley.android.clone.naver.mimicwebtoon.R
@@ -21,17 +23,9 @@ class TestActivity: AppCompatActivity(), NestedScrollView.OnScrollChangeListener
         binding = TestBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        setSupportActionBar(binding.toolbar)
-        supportActionBar?.hide()
+        initActionBar()
+        initImageBanner()
 
-        val toolbar = layoutInflater.inflate(R.layout.custom_actionbar, null)
-        supportActionBar?.customView = toolbar
-        supportActionBar?.setDisplayShowTitleEnabled(false)
-        supportActionBar?.setDisplayShowHomeEnabled(false)
-        supportActionBar?.setDisplayHomeAsUpEnabled(false)
-        supportActionBar?.setDisplayShowCustomEnabled(true)
-
-        binding.scrollview.setOnScrollChangeListener(this)
     }
 
     override fun onStart() {
@@ -52,14 +46,58 @@ class TestActivity: AppCompatActivity(), NestedScrollView.OnScrollChangeListener
     ) {
         if(scrollY == 0){
             supportActionBar?.hide()
+            hideImageBanner()
         } else {
             supportActionBar?.show()
+            showImageBanner()
         }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.appbar, menu)
         return true
+    }
+
+    private fun initActionBar(){
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.hide()
+
+        val toolbar = layoutInflater.inflate(R.layout.custom_actionbar, null)
+        supportActionBar?.customView = toolbar
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+        supportActionBar?.setDisplayShowHomeEnabled(false)
+        supportActionBar?.setDisplayHomeAsUpEnabled(false)
+        supportActionBar?.setDisplayShowCustomEnabled(true)
+
+        binding.scrollview.setOnScrollChangeListener(this)
+    }
+
+    private fun initImageBanner(){
+        hideImageBanner()
+    }
+
+    private fun showImageBanner(){
+        binding.imageBanner.visibility = View.VISIBLE
+
+        val constraintSet = ConstraintSet()
+        constraintSet.clone(binding.constraintLayout)
+        constraintSet.connect(binding.scrollview.id, ConstraintSet.BOTTOM, binding.imageBanner.id, ConstraintSet.TOP)
+        constraintSet.connect(binding.imageBanner.id, ConstraintSet.TOP, binding.scrollview.id, ConstraintSet.BOTTOM)
+        constraintSet.connect(binding.imageBanner.id, ConstraintSet.BOTTOM, binding.gnbBar.id, ConstraintSet.TOP)
+        constraintSet.connect(binding.gnbBar.id, ConstraintSet.TOP, binding.imageBanner.id, ConstraintSet.BOTTOM)
+
+        constraintSet.applyTo(binding.constraintLayout)
+    }
+
+    private fun hideImageBanner(){
+        binding.imageBanner.visibility = View.GONE
+
+        val constraintSet = ConstraintSet()
+        constraintSet.clone(binding.constraintLayout)
+        constraintSet.connect(binding.scrollview.id, ConstraintSet.BOTTOM, binding.gnbBar.id, ConstraintSet.TOP)
+        constraintSet.connect(binding.gnbBar.id, ConstraintSet.TOP, binding.scrollview.id, ConstraintSet.BOTTOM)
+
+        constraintSet.applyTo(binding.constraintLayout)
     }
 
 }
