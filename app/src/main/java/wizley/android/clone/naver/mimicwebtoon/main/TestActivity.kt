b25008e1e99105fd83ae3a7a110cac25c8843cce
wizleysw <wizley@kakao.com>
2020-08-20 19:43:43 +0900

    parse lists from NaverWebtoon using Jsoup
    Reviewed by Wizley.
    2020.08.20.
    
    Parse data from webtoon to show list using jsoup is implemented. It gets list of cartoon of days and that of episodes.

--- app/src/main/java/wizley/android/clone/naver/mimicwebtoon/main/TestActivity.kt
+++ app/src/main/java/wizley/android/clone/naver/mimicwebtoon/main/TestActivity.kt
@@ -1,24 +1,65 @@
 package wizley.android.clone.naver.mimicwebtoon.main
 
-import android.annotation.SuppressLint
-import android.graphics.Color
-import android.graphics.Rect
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.util.Log
-import android.view.Menu
-import android.view.MenuItem
-import android.view.View
-import android.widget.TextView
+
 import androidx.appcompat.app.AppCompatActivity
-import androidx.constraintlayout.widget.ConstraintSet
-import androidx.core.view.isVisible
-import androidx.core.widget.NestedScrollView
-import com.google.android.material.bottomnavigation.BottomNavigationView
-import kotlinx.android.synthetic.main.test.*
-import wizley.android.clone.naver.mimicwebtoon.R
-import wizley.android.clone.naver.mimicwebtoon.databinding.TestBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import org.jsoup.Jsoup
+import kotlin.coroutines.CoroutineContext
 
 class TestActivity: AppCompatActivity(){
+    private val TAG = "TEST"
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
 
+        parseEpisodeList("651673")
+
+    }
+
+    private fun parseWebtoonList(day: String){
+        CoroutineScope(Dispatchers.Default).launch{
+            val doc = Jsoup.connect("https://comic.naver.com/webtoon/weekdayList.nhn?week=$day").get()
+            val elements = doc.select("ul.img_list li")
+
+            elements.forEachIndexed{index, elem ->
+                val no = elem.select("a").attr("href").split("titleId=", "&weekday")[1]
+                val title = elem.select("a").attr("title")
+                val img = elem.select("img").attr("src")
+                val author = elem.select("dd.desc a").text()
+                val star = elem.select("div.rating_type strong").text()
+
+                Log.e(TAG, "-------------------")
+                Log.e(TAG, no)
+                Log.e(TAG, title)
+                Log.e(TAG, img)
+                Log.e(TAG, author)
+                Log.e(TAG, star)
+            }
+        }
+    }
+
+    private fun parseEpisodeList(no: String){
+        CoroutineScope(Dispatchers.Default).launch{
+            val doc = Jsoup.connect("https://comic.naver.com/webtoon/list.nhn?titleId=$no").get()
+            val elements =  doc.select("tr")
+
+            elements.forEachIndexed{index, elem ->
+                val img = elem.select("td a img").attr("src")
+                val title = elem.select("td a img").attr("title")
+                val rate =  elem.select("div.rating_type strong").text()
+
+                if(img.isNotEmpty() and title.isNotEmpty() and rate.isNotEmpty()) {
+                    Log.e(TAG, img)
+                    Log.e(TAG, title)
+                    Log.e(TAG, rate)
+                }
+            }
+        }
+    }
 }
